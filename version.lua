748b74b42dae372a2e899e1bad3f09df188e5243
ry <75577692+ry361@users.noreply.github.com>
2026-03-18 10:36:45 +0300

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,11 +1,14 @@
 return {
     esx = {
         multicharacter = {
-            version = "1.0.0",
-            releaseDate = nil,
-            critical = false,
+            version = "1.1.1",
+            releaseDate = "2026-03-18",
+            critical = true,
             download = "https://portal.cfx.re/assets/granted-assets",
-            changelog = {},
+            changelog = {
+                { text = "Fixed an issue where sex and dateofbirth were not saved to the database", link = nil },
+                { text = "Resolved character loading issue after creation (Wake Up crash)", link = nil },
+            },
             missingFeatures = {},
             removed = {}
         }
