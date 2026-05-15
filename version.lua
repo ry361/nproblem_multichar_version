7cb8eed88acb5b4bfb7f5bdf45c75b3bee81528c
ry <75577692+ry361@users.noreply.github.com>
2026-05-15 04:56:21 +0300

    Update version.lua

--- version.lua
+++ version.lua
@@ -14,12 +14,12 @@ return {
     },
     qb = {
         multicharacter = {
-            version = "1.0.1",
-            releaseDate = "2026-05-04",
+            version = "1.0.2",
+            releaseDate = "2026-05-15",
             critical = false,
             download = "https://portal.cfx.re/assets/granted-assets",
             changelog = {
-                { text = "Fixed an issue where weather sync was not properly re-enabled after spawn, causing weather to stay locked.", link = nil },
+                { text = ""Added relog option.", link = nil },
             },
             missingFeatures = {},
             removed = {}
